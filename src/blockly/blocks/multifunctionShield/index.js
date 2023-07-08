93c6dd5d13caedfb0ed39389bfe55a89463c63a3
Nicolás Streri <nicolas@streri.com>
2023-07-08 04:31:29 -0300

    feat(Block): MultiFunction Shield: change_led_status block

--- src/blockly/blocks/multifunctionShield/index.js
+++ src/blockly/blocks/multifunctionShield/index.js
@@ -1,3 +1,5 @@
 import './display_clean';
 import './display_write_int';
 import './display_write_float';
+import './change_led_status';
+
--- src/config/arduinoToolbox.js
+++ src/config/arduinoToolbox.js
@@ -93,6 +93,8 @@ export default {
                 { kind: "block", type: "multi_shield_display_clean"},
                 { kind: "block", type: "multi_shield_display_write_int"},
                 { kind: "block", type: "multi_shield_display_write_float"},
+                { kind: "label", text: "Otros Sensores y Actuadores"},
+                { kind: "block", type: "multi_shield_change_led_status"}
             ]
         },
         
