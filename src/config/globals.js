6c5ee022e632c8dd7031dd8ab4820d73bd907ada
Nicolás Streri <nicolas@streri.com>
2023-07-15 18:16:09 -0300

    fix: Add missing endpoint

--- src/config/globals.js
+++ src/config/globals.js
@@ -7,3 +7,4 @@ export const APP_COPYRIGHT_MSG = `${APP_NAME} es un proyecto libre desarrollado 
 export const UPDATE_LIBRARIES_URL = "[SERVER_DOWNLOAD_URL_HERE]";
 export const GET_GROUP_DATA_URL = "[SERVER_GET_GROUP_DATA_URL_HERE use %GID to replace with current group id]";
 export const SET_GROUP_MEMBERS_URL = "[SET_GROUP_MEMBERS_URL use %GID to replace with current group id]";
+export const MARK_FOR_REVIEW_URL = "[use %GID to replace with current group id and %PID to replace with current project]";
