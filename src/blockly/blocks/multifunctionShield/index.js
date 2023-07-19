93733036ddca22cef1e221e77be9592759e2e9f1
Nicolás Streri <nicolas@streri.com>
2023-07-18 21:25:33 -0300

    feat(Block): show boolean values

--- src/blockly/blocks/multifunctionShield/index.js
+++ src/blockly/blocks/multifunctionShield/index.js
@@ -2,6 +2,7 @@ import './display_clean';
 import './display_write';
 import './display_write_int';
 import './display_write_float';
+import './display_write_bool';
 import './change_led_status';
 import './get_button_status';
 import './buzzer_tone';
