9e61014c2b358a7ad0c6bcfd49c105279a4eed9f
Nicolás Streri <nicolas@streri.com>
2023-07-21 05:51:51 -0300

    fix: missing to look for the initialization of the variable in the Arduino setup

--- src/blockly/variables/check_var_initialization_extension.js
+++ src/blockly/variables/check_var_initialization_extension.js
@@ -4,17 +4,22 @@ import { Events, Extensions } from "blockly";
  * Based on https://github.com/google/blockly/blob/9efc0d7dd0cd975da25a633760abe0a0ad753415/blocks/loops.ts#L337C6-L337C6
  */
 const checkVariableInitializationMixin = {
+
+    isInitializationBlock: function (block, varIDToCheck){
+      if (!block.disabled && block.type && block.type.startsWith("variables_set_")) {
+        //Check var id
+        const blockVarID = block.getField("VAR").getValue();
+        if(blockVarID == varIDToCheck){
+          return true;
+        }
+      }
+      return false;
+    },
     
     getClosestInitialization: function (startBlock, varIDToCheck) {
         let block = startBlock;
         do {
-            if (!block.disabled && block.type && block.type.startsWith("variables_set_")) {
-              //Check var id
-              const blockVarID = block.getField("VAR").getValue();
-              if(blockVarID == varIDToCheck){
-                return block;
-              }
-            }
+            if(this.isInitializationBlock(block, varIDToCheck)) return block;
             block = block.getParent();
         } while (block);
         return null;
@@ -27,6 +32,35 @@ const checkVariableInitializationMixin = {
       } while( block && block.getPreviousBlock() == null);
       return block;
     },
+
+    checkOnSetup: function(varIDToCheck){
+      const rootBlock = this.getRootBlock();
+      if(rootBlock.type != "board_setup_loop") return null;
+
+      let block = rootBlock.getInputTargetBlock("SETUP");
+      if(!block) return null;
+
+      do{
+        if(this.isInitializationBlock(block, varIDToCheck)) return block;
+        block = block.getNextBlock();
+      }while(block);
+      return null;
+    },
+
+    checkInitialization: function(){
+      const varIDToCheck = this.getField("VAR").getValue();
+
+      //searches the block tree for the closest initialization of the variable
+      const nearestStatementBlock = this.getNearestStatementBlock();
+      if(nearestStatementBlock && !!this.getClosestInitialization(nearestStatementBlock.getParent(), varIDToCheck)){
+        return true;
+      }
+
+      // look for the initialization of the variable in the Arduino setup
+      if(this.checkOnSetup(varIDToCheck)) return true;
+
+      return false;
+    },
   
     /**
      * Called whenever anything on the workspace changes.
@@ -37,18 +71,11 @@ const checkVariableInitializationMixin = {
       // Don't change state if:
       //   * It's at the start of a drag.
       //   * It's not a move event.
-      if (this.disabled || !ws.isDragging || ws.isDragging() || (e.type !== Events.BLOCK_MOVE && e.type !== Events.BLOCK_CHANGE)) {
+      if (!ws.isDragging || ws.isDragging() || (e.type !== Events.BLOCK_MOVE && e.type !== Events.BLOCK_CHANGE)) {
         return;
       }
-      const varIDToCheck = this.getField("VAR").getValue();
 
-      const nearestStatementBlock = this.getNearestStatementBlock();
-      console.log(nearestStatementBlock);
-      let initialized = false;
-      if(nearestStatementBlock){
-        initialized = !!this.getClosestInitialization(nearestStatementBlock.getParent(), varIDToCheck);
-      }
-    
+      const initialized = this.checkInitialization();
       this.setWarningText(
         initialized ? null : "Se debe guardar un valor en la variable antes de usarla."
       );
